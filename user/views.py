0044c3d6f98fa9aa196b557d45923e724f6adaf4
KietCSE <ptkiet170104@gmail.com>
2024-07-24 22:04:44 +0700

    init

---
+++ user/views.py
@@ -0,0 +1,8 @@
+from django.template import loader
+from django.http import HttpResponse
+from django.shortcuts import render 
+
+# Create your views here.
+def Home(request):
+    template = loader.get_template('index.html')
+    return  HttpResponse(template.render())

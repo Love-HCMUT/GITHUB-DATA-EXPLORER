75803d375066d448aaafe01b6afe54dfee4f9c85
Hailam2104 <lam.duong210425@hcmut.edu.vn>
2024-08-12 20:48:31 +0700

    add **.pyc

--- user/src/analysis/user_analysis.py
+++ user/src/analysis/user_analysis.py
@@ -6,10 +6,13 @@ async def get_languages(username, TOKEN):
     :param username: The handle for the GitHub user account.
     :return: a dictionary contains languages and corresponding count.
     """
+    DEMAND = 10
     repos = await fetch.fetch_repos_name(username, TOKEN)
     languages = {}
     for repo in repos:
         data = await fetch.fetch_repo_languages(username, repo, TOKEN)
         for key in data:
             languages[key] = languages.get(key, 0) + 1
+    if (len(languages) < DEMAND): DEMAND = len(languages)
+    languages = dict(sorted(languages.items(), key = lambda item: item[1], reverse = True)[:DEMAND])
     return languages
